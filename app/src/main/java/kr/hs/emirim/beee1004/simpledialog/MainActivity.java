8c285c7f392f6c47300d5eef351ec4afb4d53f12
Eunbee Hong <hebee935@gmail.com>
2016-06-07 20:48:53 +0900

    #4 edit

--- app/src/main/java/kr/hs/emirim/beee1004/simpledialog/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/beee1004/simpledialog/MainActivity.java
@@ -1,18 +1,21 @@
 package kr.hs.emirim.beee1004.simpledialog;
 
+import android.content.DialogInterface;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
-
+    String[] items={"젤리빈","킷켓","롤리팝","마쉬멜로"};
+    Button butText;
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button butText = (Button)findViewById(R.id.but_text);
+        butText = (Button)findViewById(R.id.but_text);
         butText.setOnClickListener(this);
     }
 
@@ -23,11 +26,22 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
      */
     @Override
     public void onClick(View v) {
+
         AlertDialog.Builder dialog = new AlertDialog.Builder(this);
         dialog.setTitle("First Dialog");
-        dialog.setMessage("This is message part.");
+        dialog.setItems(items, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                butText.setText(items[which]);
+            }
+        });
         dialog.setIcon(R.drawable.first_icon);
-        dialog.setPositiveButton("OK",null);
+        dialog.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Toast.makeText(MainActivity.this, "대화상자의 확인 버튼을 클릭했어용~", Toast.LENGTH_LONG).show();
+            }
+        });
         dialog.show();
     }
 }
